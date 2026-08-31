07970e1308f3ad0700bd35192790fbcc12313d29
KAVIN A <sgkavinjonathan@gmail.com>
2026-08-31 09:38:43 +0530

    good

---
+++ zombiegame.py
@@ -0,0 +1,68 @@
+"""n = int(input())
+zombies = list(map(int, input().split()))
+B = int(input())
+
+for zi in zombies:
+    if B < zi:
+        print("NO")
+        break
+
+    B -= (zi % 2) + (zi // 2)
+else:
+   print("YES")
+"""
+"""n, B = map(int, input().split())
+zombies = list(map(int, input().split()))
+
+for zi in zombies:
+    if B < zi:
+        print("NO")
+        break
+
+    B -= (zi % 2) + (zi // 2)
+else:
+    print("YES")
+"""
+"""n = int(input())
+B = int(input())
+zombies = list(map(int, input().split()))
+
+for zi in zombies:
+    if B < zi:
+        print("NO")
+        break
+
+    B -= (zi % 2) + (zi // 2)
+else:
+    print("YES")
+"""
+"""n = int(input("Enter the number of zombies: "))
+B = int(input("Enter Bob's energy: "))
+
+zombies = list(map(int, input("Enter the energy of each zombie: ").split()))
+
+for zi in zombies:
+    if B < zi:
+        print("NO")
+        break
+
+    B -= (zi % 2) + (zi // 2)
+else:
+    print("YES")
+"""
+n = int(input("Enter the number of zombies: "))
+B = int(input("Enter Bob's energy: "))
+
+zombies = list(map(int, input("Enter zombie energies: ").split()))
+
+if len(zombies) != n:
+    print("Please enter exactly", n, "zombie energies.")
+else:
+    for zi in zombies:
+        if B < zi:
+            print("NO")
+            break
+
+        B -= (zi % 2) + (zi // 2)
+    else:
+        print("YES")
